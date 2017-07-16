2f110e8763c43b10e7b1735203a9e516f393a8eb
Aitor Martin <aitor@martinh.es>
2017-07-17 00:15:07 +0200

    Editing exclusion for variables

--- mysql_options.py
+++ mysql_options.py
@@ -4,5 +4,5 @@ F = open("/tmp/mysql_options","r")
 OPTION = open("/etc/mysql/mysql.conf.d/mysql_options.cnf","w") 
 OPTION.write("[mysqld]")
 for line in F:
-	if line != "" and line.find("MASTER_ENV") == -1: 
+	if line != "" and line.find("ENV") == -1: 
 		OPTION.write(line.replace('MYSQL_OPTION_',''))
